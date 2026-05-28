182f6fe512aac5ab42bde8e5ddf748b297406251
Pratik Chavan <106006986+PratikChavan19@users.noreply.github.com>
2026-05-28 23:01:12 +0530

    Add files via upload

---
+++ Program1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include<vector>
+
+using namespace std;
+
+int knapsackRec(int W, vector<int> &val, vector<int> &wt, int n) {
+
+    // Base Case
+    if (n == 0 || W == 0)
+        return 0;
+
+    int pick = 0;
+
+    // Pick nth item if it does not exceed the capacity of knapsack
+    if (wt[n - 1] <= W)
+        pick = val[n - 1] + knapsackRec(W - wt[n - 1], val, wt, n - 1);
+    
+    // Don't pick the nth item
+    int notPick = knapsackRec(W, val, wt, n - 1);
+     
+    return max(pick, notPick);
+}
+
+int knapsack(int W, vector<int> &val, vector<int> &wt) {
+    int n = val.size();
+    return knapsackRec(W, val, wt, n);
+}
+
+int main() {
+//Driver Code Starts
+    vector<int> val = {1, 2, 3};
+    vector<int> wt = {4, 5, 1};
+    int W = 4;
+
+    cout << knapsack(W, val, wt) << endl;
+    return 0;
+}
